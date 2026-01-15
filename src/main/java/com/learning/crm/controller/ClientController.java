9eb65c94346ef632b64e5d16229a6e304e1d63f8
Mateus Torres <mmtorres230726@gmail.com>
2026-01-15 13:42:51 -0300

    adding the user model and changing it to correspond to the correct CRM logic where the client is the product

---
+++ src/main/java/com/learning/crm/controller/ClientController.java
@@ -0,0 +1,4 @@
+package com.learning.crm.controller;
+
+public class ClientController {
+}
---
+++ src/main/java/com/learning/crm/models/User.java
@@ -0,0 +1,23 @@
+package com.learning.crm.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String email;
+    private String passwordHash;
+
+}
