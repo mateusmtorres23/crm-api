20521061bae204220533afd0af5d1387a20e2ba8
Mateus Torres <mmtorres230726@gmail.com>
2026-01-17 09:58:42 -0300

    creating the generate token method

--- src/main/java/com/learning/crm/security/TokenService.java
+++ src/main/java/com/learning/crm/security/TokenService.java
@@ -1,7 +1,14 @@
 package com.learning.crm.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.learning.crm.models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 
 @Component
 public class TokenService {
@@ -9,4 +16,17 @@ public class TokenService {
     @Value("${api.security.jwt.secret-key}")
     private String secret;
 
+    public String generateToken(User user) {
+
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        Algorithm algorithm = Algorithm.HMAC256(keyBytes);
+
+        return JWT.create()
+                .withClaim("userId", user.getId())
+                .withSubject(user.getEmail())
+                .withExpiresAt(Instant.now().plus(15, ChronoUnit.MINUTES))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+
+    }
 }
