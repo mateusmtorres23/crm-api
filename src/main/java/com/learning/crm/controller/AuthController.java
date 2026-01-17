064a94a65f81d02d41a15f080da7f4e9c253702c
Mateus Torres <mmtorres230726@gmail.com>
2026-01-17 09:17:11 -0300

    creating the controller with placeholder methods

--- src/main/java/com/learning/crm/controller/AuthController.java
+++ src/main/java/com/learning/crm/controller/AuthController.java
@@ -1,12 +1,39 @@
 package com.learning.crm.controller;
 
+import com.learning.crm.dto.request.LoginRequest;
+import com.learning.crm.dto.request.RegisterRequest;
+import com.learning.crm.dto.response.LoginResponse;
+import com.learning.crm.dto.response.RegisterResponse;
+import com.learning.crm.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
 
+    public  AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> registerUser() {
+
+
+
+        return null;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login() {
+
+
+
+        return null;
+    }
 
 }
