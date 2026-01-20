3dbb711f287481737555168ca671d0389c083546
Mateus Torres <mmtorres230726@gmail.com>
2026-01-19 22:46:07 -0300

    adding the logic for the endpoints

--- src/main/java/com/learning/crm/controller/AuthController.java
+++ src/main/java/com/learning/crm/controller/AuthController.java
@@ -5,8 +5,10 @@ import com.learning.crm.dto.request.RegisterRequest;
 import com.learning.crm.dto.response.LoginResponse;
 import com.learning.crm.dto.response.RegisterResponse;
 import com.learning.crm.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,19 +23,27 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> registerUser() {
+    public ResponseEntity<String> registerUser(@RequestBody RegisterRequest request) {
 
-
-
-        return null;
+        try {
+            authService.userRegister(request);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("User registered successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login() {
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
 
-
-
-        return null;
+        try{
+            LoginResponse response = authService.userLogin(request);
+            return ResponseEntity.ok(response.token());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid email or password");
+        }
     }
-
 }
---
+++ src/main/java/com/learning/crm/security/JWTUserData.java
@@ -0,0 +1,4 @@
+package com.learning.crm.security;
+
+public record JWTUserData() {
+}
