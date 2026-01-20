8908e0c479c0c93bafda0400aa65fb63f2be1543
Mateus Torres <mmtorres230726@gmail.com>
2026-01-20 13:07:26 -0300

    transfering the user details service to other class to avoid circular dependencies

--- src/main/java/com/learning/crm/security/DomainUserDetailsService.java
+++ src/main/java/com/learning/crm/security/DomainUserDetailsService.java
@@ -1,4 +1,23 @@
 package com.learning.crm.security;
 
-public class DomainUserDetailsService {
+import com.learning.crm.repository.UserRepository;
+import lombok.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DomainUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public DomainUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
 }
