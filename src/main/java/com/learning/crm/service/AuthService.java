ef697a11733aafdecd8b0ac62263c336ba59a043
Mateus Torres <mmtorres230726@gmail.com>
2026-01-20 13:08:17 -0300

    removing the user details service part from AuthService

--- src/main/java/com/learning/crm/service/AuthService.java
+++ src/main/java/com/learning/crm/service/AuthService.java
@@ -9,14 +9,11 @@ import com.learning.crm.repository.UserRepository;
 import com.learning.crm.security.TokenService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthService implements UserDetailsService {
+public class AuthService{
 
     private UserRepository userRepository;
     private AuthenticationManager authenticationManager;
@@ -72,10 +69,4 @@ public class AuthService implements UserDetailsService {
         return new LoginResponse(token);
 
     }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
-    }
-
 }
