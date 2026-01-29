089168da18f6d704369cdc42e8171a22cb4f61cb
Mateus Torres <mmtorres230726@gmail.com>
2026-01-28 21:57:07 -0300

    creating a query method for better performance when presenting the users

--- src/main/java/com/learning/crm/repository/ClientRepository.java
+++ src/main/java/com/learning/crm/repository/ClientRepository.java
@@ -1,12 +1,17 @@
 package com.learning.crm.repository;
 
 import com.learning.crm.models.Client;
+import com.learning.crm.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client, String> {
 
     boolean existsByEmailOrPhone(String email, String phone);
+
+    @Query("SELECT c FROM Client c LEFT JOIN FETCH c.notes WHERE c.user = :user")
+    List<Client> findAllByUserWithNotes(User user);
 
 }
