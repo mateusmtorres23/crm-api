330d2a29a50c34a4880d75a137ff82109c17f6eb
Mateus Torres <mmtorres230726@gmail.com>
2026-01-29 10:19:39 -0300

    creating the endpoints for the client crud

--- src/main/java/com/learning/crm/controller/ClientController.java
+++ src/main/java/com/learning/crm/controller/ClientController.java
@@ -1,23 +1,121 @@
 package com.learning.crm.controller;
 
+import com.learning.crm.dto.client.request.AddNoteRequest;
+import com.learning.crm.dto.client.request.CreateClientRequest;
+import com.learning.crm.dto.client.response.ClientDetailsResponse;
+import com.learning.crm.dto.client.response.ClientInfoResponse;
+import com.learning.crm.dto.client.response.CreateClientResponse;
 import com.learning.crm.models.Client;
+import com.learning.crm.models.User;
+import com.learning.crm.repository.ClientRepository;
+import com.learning.crm.repository.UserRepository;
+import com.learning.crm.security.JWTUserData;
+import com.learning.crm.service.ClientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static com.learning.crm.models.ClientStatus.*;
+
 @RestController
-@RequestMapping("api/clients")
+@RequestMapping("/clients")
 public class ClientController {
 
-    @GetMapping
-    public List<Client> getClients() {
-        List<Client> clients = new ArrayList<>();
+    private final ClientService clientService;
+    private final UserRepository userRepository;
+    private final ClientRepository clientRepository;
 
-        return clients;
+    public ClientController(ClientService clientService, UserRepository userRepository, ClientRepository clientRepository) {
+        this.clientService = clientService;
+        this.userRepository = userRepository;
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping
+    public List<ClientInfoResponse> getClients(@AuthenticationPrincipal JWTUserData userData) {
+
+        User user = userRepository.findById(userData.userId())
+                .orElseThrow(() -> new IllegalArgumentException("Your account no longer exists"));
+
+        List<Client> clients = clientRepository.findAllByUserWithNotes(user);
+
+        return clients.stream()
+                .map(client -> new ClientInfoResponse(
+                        client.getId(),
+                        client.getName(),
+                        client.getPhone(),
+                        client.getEmail(),
+                        client.getStatus(),
+                        client.getNotes().size()
+                ))
+                .toList();
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<ClientDetailsResponse> getClientById(@PathVariable String clientId) {
+
+        return ResponseEntity.ok().body(clientService.getClientDetails(clientId));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateClientResponse> postClient(@RequestBody CreateClientRequest request,
+                                        @AuthenticationPrincipal JWTUserData userData) {
+
+            String userId = userData.userId();
+            Client client = clientService.createClient(request, userId);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(new CreateClientResponse(
+                    client.getName(),
+                    client.getEmail(),
+                    client.getPhone()
+            ));
+    }
+
+    @DeleteMapping("/{clientId}")
+    public ResponseEntity<String> deleteClient(@PathVariable String clientId) {
+        clientService.deleteClient(clientId);
+
+        return  ResponseEntity.ok().body("Client deleted successfully");
+    }
+
+    @PutMapping("/{clientId}")
+    public ResponseEntity<CreateClientResponse> editClient(@RequestBody CreateClientRequest request,
+                           @PathVariable String clientId) {
+        clientService.editClient(request, clientId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CreateClientResponse(
+                        request.name(),
+                        request.email(),
+                        request.phone()
+                )
+        );
+    }
+
+    @PostMapping("/{clientId}/notes")
+    public ResponseEntity<String> addNoteToClient(@PathVariable String clientId,
+                                                  @RequestBody AddNoteRequest request) {
+        clientService.addNoteToClient(clientId, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Note added successfully");
+    }
+
+    @PatchMapping("/{clientId}/status")
+    public ResponseEntity<String> changeStatus(@PathVariable String clientId,
+                                               @RequestParam String status) {
+        clientService.changeStatus(clientId, switch (status.toUpperCase()) {
+            case "LEAD" -> LEAD;
+            case "PROSPECT" -> PROSPECT;
+            case "CUSTOMER" -> CUSTOMER;
+            case "ARCHIVED" -> ARCHIVED;
+            default -> throw new IllegalArgumentException("Invalid status value");
+        });
+
+        return ResponseEntity.ok().body("Client status updated successfully");
     }
 
 }
